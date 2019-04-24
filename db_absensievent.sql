3f442ec6bec40527367fd35209ff1d8cdac989ea
Naufal Farras <farrasgopal13@gmail.com>
2019-04-25 00:22:50 +0700

    Add files via upload
    absensi rar silahkan di extract langsung di htdoc, dan mohon update db nya dengan sql yang baru. terima kasih :)

--- db_absensievent.sql
+++ db_absensievent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2019 at 01:56 AM
+-- Generation Time: Apr 24, 2019 at 07:21 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -21,6 +21,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_absensievent`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absen`
+--
+
+CREATE TABLE `absen` (
+  `id_absen` int(11) NOT NULL,
+  `id_prodi` int(11) NOT NULL,
+  `id_kelas` int(10) NOT NULL,
+  `absen` int(5) NOT NULL,
+  `Masuk` datetime DEFAULT CURRENT_TIMESTAMP,
+  `Pulang` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `absen`
+--
+
+INSERT INTO `absen` (`id_absen`, `id_prodi`, `id_kelas`, `absen`, `Masuk`, `Pulang`) VALUES
+(1, 2, 5, 21, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 2, 5, 21, '2019-04-24 16:31:04', '2019-04-24 16:31:04'),
+(3, 1, 17, 5, '2019-04-24 18:08:00', '2019-04-24 18:08:00');
 
 -- --------------------------------------------------------
 
@@ -50,18 +74,71 @@ INSERT INTO `jurusan` (`id_jurusan`, `nama_jurusan`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `kelas` char(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `kelas`) VALUES
+(1, '1A'),
+(2, '1B'),
+(3, '1C'),
+(4, '1D'),
+(5, '1E'),
+(6, '1F'),
+(7, '1G'),
+(8, '2A'),
+(9, '2B'),
+(10, '2C'),
+(11, '2D'),
+(12, '2E'),
+(13, '2F'),
+(14, '2G'),
+(15, '3A'),
+(16, '3B'),
+(17, '3C'),
+(18, '3D'),
+(19, '3E'),
+(20, '3F'),
+(21, '3G'),
+(22, '4A'),
+(23, '4B'),
+(24, '4C'),
+(25, '4D'),
+(26, '4E'),
+(27, '4F'),
+(28, '4G');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `mahasiswa`
 --
 
 CREATE TABLE `mahasiswa` (
   `nim` varchar(20) NOT NULL,
   `nama` varchar(20) NOT NULL,
-  `kelas` char(5) NOT NULL,
+  `id_kelas` int(10) NOT NULL,
   `absen` int(5) NOT NULL,
   `id_jurusan` int(11) NOT NULL,
   `id_prodi` int(11) NOT NULL,
   `status` enum('Alpha','Hadir') DEFAULT 'Alpha'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `id_kelas`, `absen`, `id_jurusan`, `id_prodi`, `status`) VALUES
+('1831710012', 'Aldi', 1, 10, 3, 10, 'Alpha'),
+('1831710129', 'Farras', 1, 22, 1, 1, 'Alpha');
 
 -- --------------------------------------------------------
 
@@ -102,9 +179,55 @@ INSERT INTO `prodi` (`id_prodi`, `id_jurusan`, `nama_prodi`) VALUES
 (20, 7, 'D3 Administrasi Bisnis'),
 (21, 8, 'D3 Bahasa Inggris');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prodii`
+--
+
+CREATE TABLE `prodii` (
+  `id_prodi` int(11) NOT NULL,
+  `nama_prodi` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `prodii`
+--
+
+INSERT INTO `prodii` (`id_prodi`, `nama_prodi`) VALUES
+(1, 'D3 Manajemen Informatika'),
+(2, 'D4 Teknik Informatika'),
+(3, 'D3 Teknik Mesin'),
+(4, 'D4 Teknik Mesin Produksi dan Perawatan'),
+(5, 'D4 Teknik Otomotif Elektronik'),
+(6, 'D3 Teknik Elektronika'),
+(7, 'D3 Teknik Listrik Industri'),
+(8, 'D3 Teknik Telekomunikasi'),
+(9, 'D4 Jaringan Telekomunikasi Digital'),
+(10, 'D4 Sistem Kelistrikan'),
+(11, 'D4 Teknik Elektronika'),
+(12, 'D3 Teknik Konstruksi Jalan, Jembatan, da'),
+(13, 'D3 Teknik Sipil'),
+(14, 'D4 Manajemen Rekayasa Konstruksi'),
+(15, 'D3 Teknik Kimia'),
+(16, 'D4 Teknik Kimia Industri'),
+(17, 'D3 Akuntansi'),
+(18, 'D4 Akuntansi Manajemen'),
+(19, 'D3 Administrasi Bisnis'),
+(20, 'D4 Manajemen Pemasaran'),
+(21, 'D3 Bahasa Inggris');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `absen`
+--
+ALTER TABLE `absen`
+  ADD PRIMARY KEY (`id_absen`),
+  ADD KEY `id_prodi` (`id_prodi`),
+  ADD KEY `id_kelas` (`id_kelas`);
 
 --
 -- Indexes for table `jurusan`
@@ -113,12 +236,19 @@ ALTER TABLE `jurusan`
   ADD PRIMARY KEY (`id_jurusan`);
 
 --
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`nim`),
   ADD KEY `id_jurusan` (`id_jurusan`),
-  ADD KEY `id_prodi` (`id_prodi`);
+  ADD KEY `id_prodi` (`id_prodi`),
+  ADD KEY `kelas` (`id_kelas`);
 
 --
 -- Indexes for table `prodi`
@@ -128,35 +258,64 @@ ALTER TABLE `prodi`
   ADD KEY `id_jurusan` (`id_jurusan`);
 
 --
+-- Indexes for table `prodii`
+--
+ALTER TABLE `prodii`
+  ADD PRIMARY KEY (`id_prodi`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `absen`
+--
+ALTER TABLE `absen`
+  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `jurusan`
 --
 ALTER TABLE `jurusan`
   MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+--
 -- AUTO_INCREMENT for table `prodi`
 --
 ALTER TABLE `prodi`
+  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `prodii`
+--
+ALTER TABLE `prodii`
   MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `absen`
+--
+ALTER TABLE `absen`
+  ADD CONSTRAINT `absen_ibfk_1` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id_prodi`);
+
+--
 -- Constraints for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id_jurusan`),
-  ADD CONSTRAINT `mahasiswa_ibfk_2` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id_prodi`);
+  ADD CONSTRAINT `mahasiswa_ibfk_3` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id_jurusan`),
+  ADD CONSTRAINT `mahasiswa_ibfk_4` FOREIGN KEY (`id_prodi`) REFERENCES `prodii` (`id_prodi`),
+  ADD CONSTRAINT `mahasiswa_ibfk_5` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`);
 
 --
 -- Constraints for table `prodi`
 --
 ALTER TABLE `prodi`
-  ADD CONSTRAINT `prodi_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id_jurusan`);
+  ADD CONSTRAINT `prodi_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id_jurusan`),
+  ADD CONSTRAINT `prodi_ibfk_2` FOREIGN KEY (`id_prodi`) REFERENCES `prodii` (`id_prodi`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
